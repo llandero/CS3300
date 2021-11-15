dcf7e3dfbcbacd43d79ae0179cfbbb70cf825f4e
luis landeros <llandero@uccs.edu>
2021-11-15 16:28:12 +0000

    changes to project.rb to make test fail

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
     # This part validates that a title and description is entered
     # when creating a project in the portfolio
-    validates_presence_of :title, :description 
+    #validates_presence_of :title, :description 
 end
