feb51d0a74a2768da3cd74bfa4860deaee690452
luis landeros <llandero@uccs.edu>
2021-11-17 16:56:47 +0000

    This removing the test comment. No real modifications

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,5 @@
 class Project < ApplicationRecord
     # This part validates that a title and description is entered
     # when creating a project in the portfolio
-    validates_presence_of :title, :description 
-    #This is a test comment
+    validates_presence_of :title, :description
 end
