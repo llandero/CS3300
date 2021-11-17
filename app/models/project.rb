28432f2a718654512dcb3c4d0ddb27fefa2afd90
luis landeros <llandero@uccs.edu>
2021-11-17 16:53:58 +0000

    This is a test to push to iteration2_5

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,5 @@ class Project < ApplicationRecord
     # This part validates that a title and description is entered
     # when creating a project in the portfolio
     validates_presence_of :title, :description 
+    #This is a test comment
 end
